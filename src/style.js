6f3deab59ebfcd1c7b8b339c52704fecad82f883
Justin <justin.a.manning@accenture.com>
2024-10-11 18:29:33 -0500

    transfer

--- src/style.js
+++ src/style.js
@@ -1,11 +1,22 @@
 
 // Menu data structure
 let menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+{text: 'about', href: '/about'},
+{text: 'catalog', href: '#', subLinks: [
+  {text: 'all', href: '/catalog/all'},
+  {text: 'top selling', href: '/catalog/top'},
+  {text: 'search', href: '/catalog/search'},
+]},
+{text: 'orders', href: '#' , subLinks: [
+  {text: 'new', href: '/orders/new'},
+  {text: 'pending', href: '/orders/pending'},
+  {text: 'history', href: '/orders/history'},
+]},
+{text: 'account', href: '#', subLinks: [
+  {text: 'profile', href: '/account/profile'},
+  {text: 'sign out', href: '/account/signout'},
+]},
+];
 
 //  Select and cache the <main> element in a variable named mainEl.
 const mainEl = document.querySelector("main")
@@ -26,7 +37,7 @@ mainEl.style.alignItems = "center"
 const topMenuEl = document.getElementById("top-menu")
 topMenuEl.style.height = "100%"
 topMenuEl.style.backgroundColor = "var(--sub-menu-bg)"
-topMenuEl.style.flex = "flex-around"
+topMenuEl.classList= "flex-around"
 
 // Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property."
 
@@ -36,4 +47,18 @@ let newEl = document.createElement("a")
  newEl.innerHTML = link.text;
  topMenuEl.append(newEl)
 })
+
+const subMenuEl = document.getElementById("sub-menu")
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "#3da4ab"
+subMenuEl.classList = "flex-around"
+subMenuEl.style.position = "absolute"
+subMenuEl.style.top = "0"
+
+const topMenuLinks = document.querySelectorAll("#top-menu a")
+
+topMenuEl.addEventListener("click", (e)=> {
+        
+}
+ )
 
